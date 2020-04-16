9dc87c87dd1a7dd40a74cfbb12dc5066bd89f63d
thachtrunghoang <thachtrunghoang@gmail.com>
2020-04-16 12:33:16 +0700

    update oop

--- oop.c
+++ oop.c
@@ -1,5 +1,5 @@
 #include "def.h"
-
+#include <stdlib.h>
 typedef struct man
 {
     char name[10];
@@ -37,20 +37,36 @@ static void doWork()
 }
 static void init_struct(man *pInitMan)
 {
+    pman = pInitMan;
     pInitMan->getName = getName;
     pInitMan->setName = setName;
     pInitMan->getAge = getAge;
     pInitMan->setAge = setAge;
     pInitMan->doWork = doWork;
 }
+
 void oop(void)
 {
-    man man;
-    pman = &man;
-    init_struct(pman);
-    man.setName("Hoang");
-    man.setAge(26);
-    man.getName();
-    man.getAge();
-    man.doWork();
+    int n=10;
+    man *ptrMan;
+    ptrMan = (struct man*)malloc(n* sizeof(man));
+    for (int i=0;i<9;i++)
+    {
+        init_struct(ptrMan+i);
+        (ptrMan+i)->setName("Hoang");
+        (ptrMan+i)->setAge(26);
+    }
+    init_struct(ptrMan+9);
+    (ptrMan+9)->setName("Thanh");
+    (ptrMan+9)->setAge(26);
+
+    init_struct(ptrMan);
+    ptrMan->getName();
+    ptrMan->getAge();
+    ptrMan->doWork();
+
+    init_struct(ptrMan+9);
+    ptrMan[9].getName();
+    ptrMan[9].getAge();
+    ptrMan[9].doWork();
 }
